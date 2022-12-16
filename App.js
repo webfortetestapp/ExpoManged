653d300f7a95f395d7368f451e5ba5e8df7bbc96
wft-dev <dev@myweb-development.com>
2022-12-16 16:04:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import 'expo-dev-client';
+
+export default function App() {
+  console.log(Constants.systemFonts);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to stytyty2</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
